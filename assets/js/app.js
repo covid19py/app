c1a626da7cb093f7799bbf18e3fb96a2da3547f4
Matias Insaurralde <matias@insaurral.de>
2020-03-22 11:34:14 -0400

    Ajustes en JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // var asu = {lat: -23.30066, lng: -57.63591}
-    var asu = {lat: -25.29066, lng: -57.50591},
+    var asu = { lat: -25.29066, lng: -57.50591 },
         map = new google.maps.Map(
-        document.getElementById('map'),
-        // {zoom: 6.2, center: asu}
-        { zoom: 12, center: asu }),
-        markers = []
-
+            document.getElementById('map'),
+            // {zoom: 6.2, center: asu}
+            { zoom: 12, center: asu }),
+        markers = {}
+    // window.map = map
     var markerSzW = 30,
         markerSzH = 30;
+    currentMarker = null;
+
     var inactiveMarkerIcon = {
         url: "/assets/img/pin_inactivo.png",
         scaledSize: new google.maps.Size(markerSzW, markerSzH),
@@ -23,13 +25,14 @@ $(document).ready(function() {
     };
 
     function resetMarkerIcons() {
-        for(i in markers) {
-            var m = markers[i]
+        for (id in markers) {
+            var m = markers[id]
             m.setIcon(inactiveMarkerIcon)
         }
     }
 
     function markerFocus(marker) {
+        currentMarker = marker
         var datos = marker.get('datos')
         resetMarkerIcons()
         $('.detalle_denuncia #tipo_denuncia').text(datos['tipo_denuncia'])
@@ -44,6 +47,9 @@ $(document).ready(function() {
         var col = baseCol.clone(),
             datos = marker.get('datos')
         col.removeClass('base_col')
+        col.addClass('denuncia_col')
+        col.data('id', datos['_id'])
+
         fechaElem = col.find(".fecha")
         fechaElem.text(datos['creado'].format('DD/MM/YY HH:mm'))
         tipoElem = col.find('.tipo')
@@ -53,27 +59,83 @@ $(document).ready(function() {
         ciudadElem = col.find('.ciudad')
         ciudadElem.text(datos['ciudad'])
         ubicarElem = col.find('a')
-        ubicarElem.on('click', function() {
+        ubicarElem.on('click', function () {
             markerFocus(marker)
         })
-        $('#tabla_denuncias').append(col)
+        $('#tabla_denuncias').prepend(col)
     }
 
-    $.get("/reporte/denuncias", function(data) {
-        for(i in data) {
-            var d = data[i]
-            d['creado'] = moment(d['creado'] * 1000)
-            var marker = new google.maps.Marker({
-                position: d['coordenadas'],
-                map: map,
-                icon: inactiveMarkerIcon
-            })
-            markers.push(marker)
-            marker.set('datos', d)
-            newCol(marker)
-            marker.addListener("click", function() {
-                markerFocus($(this)[0])
-            })
-        }
+    function removeCol(marker) {
+        var datos = marker.get('datos')
+        $('.denuncia_col').each(function (index, e) {
+            var id = $(e).data('id')
+            if (id == datos['_id']) {
+                $(e).remove()
+            }
+        })
+    }
+
+    function getBounds() {
+        boundaries = map.getBounds()
+        query = [
+            [ boundaries["Za"]["i"], boundaries["Ua"]["i"] ],
+            [ boundaries["Za"]["j"], boundaries["Ua"]["j"] ]
+        ]
+        return query
+    }
+
+    function syncMarkers() {
+        var boundaries = getBounds()
+        $.ajax({
+            url: '/reporte/denuncias',
+            type: 'POST',
+            data: JSON.stringify(boundaries),
+            dataType: 'json',
+            contentType: 'application/json'
+        }).done(function(data) {
+            ids = [];
+            for (i in data) {
+                var d = data[i],
+                    id = d['_id']
+                ids.push(id)
+                // Agregar markers nuevos:
+                if (markers[id] == null) {
+                    d['creado'] = moment(d['creado'] * 1000)
+                    point = new google.maps.LatLng(d['coordenadas'][0], d['coordenadas'][1])
+                    var marker = new google.maps.Marker({
+                        position: point,
+                        map: map,
+                        icon: inactiveMarkerIcon
+                    })
+                    markers[id] = marker
+                    marker.set('datos', d)
+                    newCol(marker)
+                    marker.addListener("click", function () {
+                        markerFocus($(this)[0])
+                    })
+                }
+            }
+            // Eliminar markers inexistentes o markers que cambiaron de estado:
+            for (id in markers) {
+                var m = markers[id]
+                if (ids.indexOf(id) == -1) {
+                    m.setMap(null)
+                    removeCol(m)
+                    delete(markers[id])
+                }
+            }
+        })
+    }
+
+    $('.atendido').on('click', function () {
+        var datos = currentMarker.get('datos'),
+            id = datos['_id']
+        var url = '/reporte/denuncias/' + id + '/atendida'
+        $.get(url, function () {
+        })
     })
+
+    // syncMarkers()
+    setInterval(syncMarkers, 2000)
+
 })
